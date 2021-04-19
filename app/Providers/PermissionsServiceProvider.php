e7bed1794dc5dd43f3c2356c3ea7c1237dccf8ca
Feisal Mombo <feisalmombo29@gmail.com>
2021-04-19 17:35:10 +0300

    fix bugs on repo

--- app/Providers/PermissionsServiceProvider.php
+++ app/Providers/PermissionsServiceProvider.php
@@ -19,7 +19,18 @@ class PermissionsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $permissions = \App\Permission::get()->map(function ($permission) {
+            Gate::define($permission->slug, function ($user) use ($permission) {
+                return $user->hasPermissionTo($permission);
+            });
+        });
+
+        Blade::directive('role', function ($role) {
+            return "<?php if(auth()->check() && auth()->user()->hasRole({$role})) :";
+        });
+        Blade::directive('endrole', function ($role) {
+            return "<?php endif; ?>";
+        });
     }
 
     /**
